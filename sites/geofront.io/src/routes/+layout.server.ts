b802bd6d71746d5dd1729da03a4e4c3cd5dae535
Jin Igarashi <igarashi.jin@water-gis.com>
2023-05-11 13:06:47 +0900

    fix: removed fs import

--- sites/geofront.io/src/routes/+layout.server.ts
+++ sites/geofront.io/src/routes/+layout.server.ts
@@ -3,9 +3,9 @@ import yaml from 'js-yaml';
 import type { Config } from '../lib/interfaces';
 
 export const load: LayoutServerLoad = async (event) => {
-	const res = await event.fetch('config.yaml')
-	const blob = await res.blob()
-	const yamlAsString = await blob.text()
+	const res = await event.fetch('config.yaml');
+	const blob = await res.blob();
+	const yamlAsString = await blob.text();
 
 	const config = yaml.load(yamlAsString) as Config;
 
